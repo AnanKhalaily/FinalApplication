9535bab4d2dd87ef1927aa97d29883d826f4de2e
ananc <anan.cena3@gmail.com>
2017-09-19 18:44:20 +0100

    Hi

--- app/src/main/java/com/example/hp1/finalapplication/MainActivity.java
+++ app/src/main/java/com/example/hp1/finalapplication/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.hp1.finalapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btlogin,btsignup;
+    TextView tvlogin,tvsignup;
+    EditText etusername,etpassword;
+    ImageView ivmyndlift;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btlogin=(Button)findViewById(R.id.btlogin);
+        btlogin.setOnClickListener(this);
+        btsignup=(Button)findViewById(R.id.btsignup);
+        btsignup.setOnClickListener(this);
+        tvlogin=(TextView)findViewById(R.id.tvlogin);
+        tvsignup=(TextView)findViewById(R.id.tvsignup);
+        etusername=(EditText)findViewById(R.id.etusername);
+        etusername=(EditText)findViewById(R.id.etpassword);
+        ivmyndlift=(ImageView)findViewById(R.id.ivmyndlift);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
