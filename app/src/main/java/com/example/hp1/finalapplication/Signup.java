5726c09734835e68bf525d16969420d85332b7fe
ananc <anan.cena3@gmail.com>
2017-10-15 12:04:58 +0100

    Ready For Exam

--- app/src/main/java/com/example/hp1/finalapplication/Signup.java
+++ app/src/main/java/com/example/hp1/finalapplication/Signup.java
@@ -10,7 +10,7 @@ public class Signup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Signup);
+        setContentView(R.layout.signup);
 
         }
     }
